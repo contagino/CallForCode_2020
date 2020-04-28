70e1ad10ba2d95b37d770354e7b0f0e9f1259751
jayanta1977 <mitra.dipan82@gmail.com>
2020-04-28 05:49:38 +0530

    contagino ui code uploaded

---
+++ contagino-ui/README.md
@@ -0,0 +1,15 @@
+# contagiNO
+IBM Call for code challange.
+
+
+Install the application
+
+npm install
+
+Build application for web
+
+ionic build --prod
+
+Build application for android
+
+ionic cordova build android
---
+++ contagino-ui/src/app/config/settings/app-settings.ts
@@ -0,0 +1,10 @@
+export class AppSettings {
+
+	public static SERVER_DEV = 'http://18.223.149.253:8090/api/v1/contagino/';
+	// public static SERVER_DEV = 'http://localhost:8100/';
+
+	// API
+	public static PANDEMIC_API = 'pandemic/getCityMap';
+	// public static PANDEMIC_API = 'assets/mock/pandamic.json';
+
+}
